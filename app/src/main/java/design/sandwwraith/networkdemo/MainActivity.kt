0e07d5f20804880a878a098520d7acb7ad4d6036
Leonid Startsev <sandwwraith@gmail.com>
2018-10-17 11:52:04 +0300

    Async task lifecycle

--- app/src/main/java/design/sandwwraith/networkdemo/MainActivity.kt
+++ app/src/main/java/design/sandwwraith/networkdemo/MainActivity.kt
@@ -11,24 +11,69 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.BufferedReader
+import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var downloadTask: DownloadPostsAsyncTask
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Thread {
-                val response = URL("https://jsonplaceholder.typicode.com/posts/1").openConnection().run {
-                    connect()
-                    getInputStream().bufferedReader().readLines().joinToString("")
-                }
-                Log.d("NETWORK_THREAD", "Response: $response")
-                text_view.text = response
-            }.start()
+        fab.setOnClickListener {
+            downloadTask = DownloadPostsAsyncTask()
+            downloadTask.execute(URL("https://jsonplaceholder.typicode.com/posts/1"))
+        }
+
+        text_view.setOnClickListener {
+            downloadTask.cancel(true)
+        }
+    }
+
+    inner class DownloadPostsAsyncTask: AsyncTask<URL, Int, String>() {
+        override fun onPreExecute() {
+            Log.d(logTag, "onPreExecute: ${Thread.currentThread().name}")
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            text_view.text = getString(R.string.progress_message, values[0])
+        }
+
+        override fun onPostExecute(result: String?) {
+            Log.d(logTag, "onPostExecute: ${Thread.currentThread().name}")
+            text_view.text = getString(R.string.result_message, result)
+        }
+
+        private val logTag = "ASYNC_TASK"
+
+        override fun onCancelled(result: String?) {
+            Log.d(logTag, "Cancelled with result: $result")
+            text_view.text = "Cancelled"
+        }
+
+        override fun onCancelled() {
+            Log.d(logTag, "Cancelled without result")
+            text_view.text = "Cancelled"
+        }
+
+        override fun doInBackground(vararg params: URL): String {
+            Log.d(logTag, "doInBackground: ${Thread.currentThread().name}")
+            publishProgress(0)
+            val response = params[0].openConnection().run {
+                connect()
+                val code = (this as? HttpURLConnection)?.responseCode
+                Log.d(logTag, "Response code: $code")
+
+                getInputStream().bufferedReader().readLines().joinToString("")
+            }
+            Log.d(logTag, "Response: $response")
+            publishProgress(50)
+            Thread.sleep(1000)
+            publishProgress(100)
+            return response
         }
     }
 
